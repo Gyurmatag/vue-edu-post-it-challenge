32dad6ac5e2f5afe08dcdfe5d6636a331ee64073
Nagy Attila (aha) <nagy.attis.81@gmail.com>
2023-11-06 21:14:20 +0100

    feat: list posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,38 @@
-<script setup>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
 import Post from "./Post.vue";
+
+import axios from "axios";
+
+interface Post {
+  posts: {
+    id: number;
+    text: string;
+    createdAt: string;
+    userId: number;
+  };
+  users: {
+    id: number;
+    name: string;
+  };
+}
+const posts = ref<Post[]>([]);
+
+function loadPosts() {
+  axios
+    .get<Post[]>("https://edge-post.vercel.app/api/posts")
+    .then((response) => {
+      posts.value = response.data;
+    });
+}
+
+onMounted(() => {
+  loadPosts();
+});
 </script>
 
 <template>
   <div class="flex flex-col space-y-4">
-    <Post />
-    <Post />
-    <Post />
-    <Post />
+    <Post v-for="post in posts" v-bind:post="post" />
   </div>
 </template>
